7c394508b93012bf12911302411c264f11d3287e
Denis Ramos <denisramos.contato@gmail.com>
2026-09-01 22:23:07 -0300

    fix: refresh production asset fingerprint

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ import StreamerDashboard from '@/pages/dashboard/StreamerDashboard'
 // Layout com header e footer
 function AppLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen" data-app-release="2026-09-02">
       <Header />
       <main className="flex-1">
         <Outlet />
